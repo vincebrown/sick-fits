425a2a125696df2416b0aabf576adea936c11f4c
Vince Brown <vince@vincebrown.me>
2019-11-05 17:50:22 -0800

    Wrote Tests for CreateItem

--- components/CreateItem.js
+++ components/CreateItem.js
@@ -78,6 +78,7 @@ class CreateItem extends Component {
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
           <Form
+            data-test="form"
             onSubmit={async e => {
               // Stop from submitting
               e.preventDefault()
